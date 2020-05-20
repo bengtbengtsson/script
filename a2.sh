6de4947b21a00184fa8063a991917136c1be0633
bengtbengtsson <bengt.bengtsson@gmail.com>
2020-05-20 17:30:58 +0200

    Update a2.sh
    hostname, hosts, etc now in place

--- a2.sh
+++ a2.sh
@@ -1,8 +1,8 @@
 # Set timezone
-# ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 
 # Sync hardware clock to system time
-# hwclock --systohc
+hwclock --systohc
 
 # Set locale
 # Uncomment en_US.UTF-8 in /etc/locale.gen
@@ -10,22 +10,23 @@
 # echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 # Set persistent keymap
-# echo "KEYMAP=sv-latin1" >> /etc/vconsole.conf
+echo "KEYMAP=sv-latin1" >> /etc/vconsole.conf
 
 # Set hostname
-# Add arch32 in /etc/hostname
+echo "arch32" >> /etc/hostname
 
 # Set hosts
 # Update /etc/hosts
-# 127.0.0.1 localhost
-# ::1 localhost
-# 127.0.1.1 arch32.localdomain arch32
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.1.1 arch32.localdomain arch32" >> /etc/hosts
 
 # Install additional packages
 # pacman -S grub networkmanager wireless_tools wpa_supplicant dialog os-prober \
 #  mtools dosfstools reflector git man sudo
 
-pacman -S --noconfirm grub networkmanager wireless_tools wpa_supplicant dialog os-prober
+pacman -S --noconfirm grub networkmanager wireless_tools wpa_supplicant dialog os-prober \
+  mtools dosfstools reflector git man sudo
   
 # Install grub  
 grub-install /dev/sda
@@ -38,8 +39,9 @@ systemctl enable NetworkManager
 passwd
 
 # Add user
-# useradd -mG wheel ben
-# passwd ben
+useradd -mG wheel ben
+passwd ben
+
 # EDITOR=vi visudo # edit wheel group 
 
 # Exit chroot
