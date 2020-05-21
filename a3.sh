af480ee34c712f5278a167e50fa3dc0e206770c8
bengtbengtsson <bengt.bengtsson@gmail.com>
2020-05-21 11:44:30 +0200

    Update a3.sh
    Moved HOOKS to a2.sh script

--- a3.sh
+++ a3.sh
@@ -1,10 +1,5 @@
 # Back after reboot into the new system
 
-# Update /etc/mkinitcpio.conf
-# HOOKS=(base systemd udev udev autodetect modconf block filesystems keyboard sd-console fsck)
-# mkinitcpio -p linux-lts # Not sure of this command
-# Maybe all this could have been done just before grub in a2.sh ?
-
 # Start network
 # nmcli dev wifi con Bengtsson password "password without qoutes" name homewifi
 # Next time use: nmcli con up id homewifi
@@ -13,9 +8,12 @@
 pacman -S xf86-video-amdgpu
 
 # Install the display server, and some more
-pacman -S xorg xorg-xinit xorg-twm xorg-clock xterm
+pacman -S xorg
+# pacman -S xorg xorg-xinit xorg-twm xorg-clock xterm
 # Test with startx
 
 # Install xfce4
 pacman -S xfce4 xfce4-goodies
 # Test with startxfce4
+
+# reboot
