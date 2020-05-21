c046cbf9fb5fa5f78afe881042c43fee9f8aaa4a
bengtbengtsson <bengt.bengtsson@gmail.com>
2020-05-21 22:11:44 +0200

    Update a2.sh
    More scripting ..

--- a2.sh
+++ a2.sh
@@ -6,8 +6,9 @@ hwclock --systohc
 
 # Set locale
 # Uncomment en_US.UTF-8 in /etc/locale.gen
-# locale-gen
-# echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+sed -i '/#en_US.UTF-8.*/c\en_US.UTF-8 UTF-8' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 # Set persistent keymap
 echo "KEYMAP=sv-latin1" >> /etc/vconsole.conf
@@ -22,15 +23,12 @@ echo "::1 localhost" >> /etc/hosts
 echo "127.0.1.1 arch32.localdomain arch32" >> /etc/hosts
 
 # Install additional packages
-# pacman -S grub networkmanager wireless_tools wpa_supplicant dialog os-prober \
-#  mtools dosfstools reflector git man sudo
-
 pacman -S --noconfirm grub networkmanager wireless_tools wpa_supplicant dialog os-prober \
-  mtools dosfstools reflector git man sudo
+  mtools dosfstools reflector git man sudo bluez bluez-utils pulseaudio pulseaudio-bluetooth cups
   
 # Update /etc/mkinitcpio.conf
-# HOOKS=(base systemd udev autodetect modconf block filesystems keyboard sd-vconsole fsck)
-# mkinitcpio -p linux-lts
+sed -i '/^HOOKS.*/c\HOOKS=(base systemd udev autodetect modconf block filesystems keyboard sd-vconsole fsck)' /etc/mkinitcpio.conf
+mkinitcpio -p linux-lts
   
 # Install grub  
 grub-install /dev/sda
@@ -38,6 +36,8 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 # Update services
 systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable org.cups.cupsd
 
 # Set root password
 echo -e "updateme\nupdateme" | passwd
@@ -47,6 +47,7 @@ useradd -mG wheel ben
 echo -e "updateme\nupdateme" | passwd ben
 
 # EDITOR=vi visudo # edit wheel group 
+echo "%wheel ALL=(ALL) ALL" | sudo EDITOR="tee -a" visudo
 
 # Exit chroot
 exit
