3ea036408dbc5ac129edb34f0eed8c045af77c87
bengtbengtsson <bengt.bengtsson@gmail.com>
2020-06-13 16:08:59 +0200

    Update a2.sh
    Adding updates for dwm, not complete

--- a2.sh
+++ a2.sh
@@ -53,13 +53,28 @@ echo "%wheel ALL=(ALL) ALL" | sudo EDITOR="tee -a" visudo
 pacman -S --noconfirm xf86-video-amdgpu
 
 # Install the display server, and some more
-pacman -S --noconfirm xorg
+# pacman -S --noconfirm xorg
 # pacman -S xorg xorg-xinit xorg-twm xorg-clock xterm
 # Test with startx
-
 # Install xfce4
-pacman -S --noconfirm xfce4 xfce4-goodies
+# pacman -S --noconfirm xfce4 xfce4-goodies
 # Test with startxfce4
+
+# Install dwm
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xrandr xorg-xsetroot chromium nitrogen picom
+cp /etc/X11/xinit/xinitrc /home/ben/.xinitrc
+sed '51,55d' /home/ben/.xinitrc
+ echo "setxkbmap se &" >> /home/ben/.xinitrc
+ echo "xrandr --output Virtual-1 --mode 1600x900 &" >> /home/ben/.xinitrc
+ echo "picom -f &" >> /home/ben/.xinitrc
+ echo "exec dwm" >> /home/ben/.xinitrc
+ 
 
 # Exit chroot
 exit
+
+# Back after reboot into the new system
+
+# Start network
+# nmcli dev wifi con Bengtsson password "password without qoutes" name homewifi
+# Next time use: nmcli con up id homewifi
